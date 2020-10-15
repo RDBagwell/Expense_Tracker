64a534b0dbeeb98f4135aecf7b7b8cd85438e3b6
Robert Bagwell <erbagwell@yahoo.com>
2020-10-14 23:35:49 -0600

    Add JS

--- js/script.js
+++ js/script.js
@@ -6,15 +6,37 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-    {id: 1, text: 'Flower', amount: -20},
-    {id: 2, text: 'Salary', amount: 300},
-    {id: 3, text: 'Book', amount: -10},
-    {id: 4, text: 'Camera', amount: 150}
-];
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 
-let transactions = dummyTransactions;
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value === '' || amount.value === ''){
+        alert('Please enter text and amount.');
+    } else {
+        const transaction = {
+            id: generateId(),
+            text: text.value,
+            amount: +amount.value
+        }
+
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+        updateVlaues();
+        updatTransactions();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+function generateId() {
+    return Math.floor(Math.random() * 10000000);
+}
 
 function addTransactionDOM(transaction){
     const sign = transaction.amount < 0 ? '-' : '+';
@@ -25,10 +47,11 @@ function addTransactionDOM(transaction){
 
     item.innerHTML = `
         ${transaction.text}<span>${sign}${Math.abs(transaction.amount)}<span>
-        <button class="delete-btn">x</button>
+        <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
     `;
 
     list.appendChild(item);
+
 }
 
 function updateVlaues() {
@@ -52,10 +75,22 @@ function updateVlaues() {
     money_minus.textContent = `$${expense}`;
 }
 
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    updatTransactions();
+    init();
+}
+
 function init() {
     list.innerHTML = '';
     transactions.forEach(addTransactionDOM)
     updateVlaues();
 }
+
+function updatTransactions() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+form.addEventListener('submit', addTransaction)
 
 init();
